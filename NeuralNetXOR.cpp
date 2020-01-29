a0dcd83aa8e31f5d0f68964aaaa15314bf9edb53
Николаев Виталий <vs.nikolaev.bmstu@gmail.com>
2020-01-29 21:40:36 +0300

    Добавьте файлы проекта.

---
+++ NeuralNetXOR.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+class Net
+{
+public:
+	Net();
+	~Net();
+
+private:
+
+};
+
+Net::Net()
+{
+}
+
+Net::~Net()
+{
+}
+
+int main()
+{
+	Net myNet(topology);
+
+	myNet.feedForward(inputVals);
+	myNet.backProp(targetVals);
+	myNet.getResults(resultVals);
+
+
+
+    std::cout << "Hello World!\n";
+}
